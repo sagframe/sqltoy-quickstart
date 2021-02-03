6efd76f998d95b496ac6bed119d2a16f3a8d373c
chenrenfei <102309713@qq.com>
2021-02-03 14:47:34 +0800

    更新

--- src/main/java/com/sqltoy/quickstart/vo/base/AbstractJsontypeShowcaeVO.java
+++ src/main/java/com/sqltoy/quickstart/vo/base/AbstractJsontypeShowcaeVO.java
@@ -4,14 +4,16 @@
 package com.sqltoy.quickstart.vo.base;
 
 import java.io.Serializable;
-import java.util.List;
-import org.sagacity.sqltoy.config.annotation.Entity;
-import java.util.ArrayList;
-import org.sagacity.sqltoy.callback.SelectFields;
-import org.sagacity.sqltoy.config.annotation.Id;
-import org.sagacity.sqltoy.config.annotation.Column;
-import com.sqltoy.quickstart.vo.StaffInfoVO;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.sagacity.sqltoy.callback.SelectFields;
+import org.sagacity.sqltoy.config.annotation.Column;
+import org.sagacity.sqltoy.config.annotation.Entity;
+import org.sagacity.sqltoy.config.annotation.Id;
+
+import com.sqltoy.quickstart.vo.StaffInfoVO;
 
 /**
  * @project sqltoy-quickstart
